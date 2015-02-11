40c152dc589b6b2d7091969a5b7041555d79ce2a
Kyle Rogers <kyle.rogers@gmail.com>
2015-02-10 18:01:26 -0600

    init
    init

---
+++ BottomsUp/BottomsUp.Web/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BottomsUp.Web.MvcApplication" Language="C#" %>
---
+++ BottomsUp/BottomsUp.Web/obj/Release/Package/PackageTmp/Scripts/custom.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    var mySlidebars = new $.slidebars();
+    $('.my-button').on('click', function () {
+        mySlidebars.slidebars.toggle('left');
+    });
+
+    $(".pws-task-item").on('click', function () {
+
+        var $self = $(this);
+        var $taskList = $self.find(".pws-task-labor-breakdown");
+
+        if ($taskList.is(":hidden")) {
+            $taskList.slideDown(250);
+        } else {
+            $taskList.hide(250);
+        }
+    });
+
+    $(".pws-task-labor-breakdown").on('click', function (e) {
+        e.stopPropagation();
+    });
+
+
+});
