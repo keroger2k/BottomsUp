bdefdbab0d2a21300911546f9c5f8479819e7dfd
Kyle Rogers <kyle.rogers@gmail.com>
2015-02-13 21:50:12 -0600

    pulling some data from database

--- BottomsUp/BottomsUp.Web/Controllers/HomeController.cs
+++ BottomsUp/BottomsUp.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BottomsUp.Core.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,17 @@ namespace BottomsUp.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DatabaseContext db;
+
+        public HomeController()
+        {
+            this.db = new DatabaseContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var proposal = db.Propsals.First();
+            return View(proposal);
         }
     }
 }
